c27b2838332ee4172147e980c401b237e2941ffc
jure339 <jure.bernjak@gmail.com>
2025-11-24 22:29:03 +0100

    menu

--- nextjs-football-dashboard/src/components/Layouts/sidebar/index.tsx
+++ nextjs-football-dashboard/src/components/Layouts/sidebar/index.tsx
@@ -24,23 +24,9 @@ export function Sidebar() {
     // );
   };
 
-  useEffect(() => {
+  
     // Keep collapsible open, when it's subpage is active
-    NAV_DATA.some((section) => {
-      return section.items.some((item) => {
-        return item.items.some((subItem) => {
-          if (subItem.url === pathname) {
-            if (!expandedItems.includes(item.title)) {
-              toggleExpanded(item.title);
-            }
-
-            // Break the loop
-            return true;
-          }
-        });
-      });
-    });
-  }, [pathname]);
+    
 
   return (
     <>
