ad59afe4de6fce2bdb1b948df0815ab6ec2d8e6f
jure339 <jure.bernjak@gmail.com>
2025-12-14 21:01:06 +0100

    Update page.tsx

--- nextjs-football-dashboard/src/app/(home)/igralci/page.tsx
+++ nextjs-football-dashboard/src/app/(home)/igralci/page.tsx
@@ -76,7 +76,7 @@ export default function DeletePlayersPage() {
   return (
     <div className="mx-auto max-w-4xl p-6">
       <h1 className="mb-6 text-2xl font-bold text-dark dark:text-white">
-        Delete Players
+        Players
       </h1>
 
       {loading && <p>Loading...</p>}
